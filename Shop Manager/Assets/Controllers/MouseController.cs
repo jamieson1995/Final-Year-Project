828ab184297562b3aa74b4933fee6f836b79d634
jamieson1995 <i7629611@bournemouth.ac.uk>
2016-11-10 16:36:08 +0000

    Coding changes and journal update.
    Finished the tile and world models basics and added the mouse controller
    class for camera movement and zooming.

---
+++ Shop Manager/Assets/Controllers/MouseController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	//World position of the mouse last frame
+	Vector3 m_lastFramePos;
+
+	//World position of the mouse this frame
+	Vector3 m_currFramePos;
+
+
+	void Update ()
+	{
+		m_currFramePos = Camera.main.ScreenToWorldPoint ( Input.mousePosition );
+
+		UpdateCameraMovement();
+
+		m_lastFramePos = Camera.main.ScreenToWorldPoint ( Input.mousePosition );
+	}
+
+	void UpdateCameraMovement()
+	{
+		//Screen Dragging
+		if ( Input.GetMouseButton ( 2 ) )
+		{
+			Vector3 diff = m_lastFramePos - m_currFramePos;
+			Camera.main.transform.Translate(diff); //Moves the camera relative to itself by the given vector.
+		}
+
+		//Camera Zooming
+		Camera.main.orthographicSize -=Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 1, 20); //This sets the zoom level to always be between 1 and 20.
+	}
+}
--- Shop Manager/Assets/Models/Tile.cs
+++ Shop Manager/Assets/Models/Tile.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class Tile {
 
+	public World m_world;
+
 	public int X { get; protected set; }
 	public int Y { get; protected set; }
 
-	public Tile(int x, int y)
+	public Tile(World _world, int _x, int _y)
 	{
-		
+		this.m_world = _world;
+		this.X = _x;
+		this.Y = _y;
 	}
 
 
